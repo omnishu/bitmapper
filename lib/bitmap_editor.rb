c5a9771aecb6faacf3fb07d4e9a2ba4b27d357f0
Nishutosh <om.nishu.sharma@gmail.com>
2018-02-04 22:33:54 +0530

    App architecture development In Progress
    Developing app architecture

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,16 +1,21 @@
-class BitmapEditor
+require_relative 'bitmapper'
 
+class BitmapEditor
   def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+    return puts 'Please provide correct file' if file.nil? || !File.exist?(file)
+    build_bitmap(file)
+  end
+
+  private
+
+  def build_bitmap(file)
+    bitmapper = Bitmapper.new
 
     File.open(file).each do |line|
       line = line.chomp
-      case line
-      when 'S'
-          puts "There is no image"
-      else
-          puts 'unrecognised command :('
-      end
+      bitmapper.execute!(line)
     end
+    # return bitmapper
+    puts bitmapper.bitmap
   end
 end
